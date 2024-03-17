621d13535870a69f27d2e64db680056354e15e0c
WeiDer <weiderwork131491@gmail.com>
2024-03-17 23:41:40 +0800

    刪除未使用圖片

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   apiServiceHost: '/',
-  //scrumApi:'http://localhost:7015/',
   redirect_uri:'https://mandate-group.com/sso',
   scrumApi:'http://220.132.157.210/',
   clientId:'693585711367-m4889m9h4p8j6r9c3hjcc1t1gspfoq8q.apps.googleusercontent.com'
