ce13efbca8ff76a203188440e361ff4c95951b16
WeiDer <weiderwork131491@gmail.com>
2024-03-18 01:48:17 +0800

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiServiceHost: '/',
-  redirect_uri:'https://mandate-group.com/sso',
   scrumApi:'http://220.132.157.210/',
+  redirect_uri:'https://mandate-group.com/sso',
   clientId:'693585711367-m4889m9h4p8j6r9c3hjcc1t1gspfoq8q.apps.googleusercontent.com'
 };
